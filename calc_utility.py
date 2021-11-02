b413c5217e25c1880856f069187704dcbe26f956
DESKTOP-N14U5N0\Sergej <agoyyes@web.de>
2021-11-02 17:43:48 +0100

    Refactoring + Normierung in calc_util rausgenommen

--- calc_utility.py
+++ calc_utility.py
@@ -9,7 +9,8 @@ from config import *
 
 def aggregate_by_docno(wordlist: pd.DataFrame, hot_encoded_wl: np.ndarray) -> ([int], [np.ndarray]):
     filtered_wl = wordlist.drop_duplicates(subset=['DOC_NUMBER'])
-    if len(filtered_wl.index) == 2:
+    wordlist_count = len(filtered_wl.index)
+    if wordlist_count == 2:
         index_set = [wordlist.index[wordlist['DOC_NUMBER'] ==
                                     docno].tolist() for docno in filtered_wl['DOC_NUMBER']]
         array = [hot_encoded_wl[x[0]:x[-1], :].toarray() for x in index_set]
@@ -17,14 +18,14 @@ def aggregate_by_docno(wordlist: pd.DataFrame, hot_encoded_wl: np.ndarray) -> ([
         aggregation = [np.divide(x, x, out=np.zeros_like(
             x), where=x != 0) for x in aggregation]
         return ([docno for docno in filtered_wl['DOC_NUMBER']], aggregation)
-    elif len(filtered_wl.index) == 1:
+    elif wordlist_count == 1:
         diag_entry = filtered_wl.at[0, 'DOC_NUMBER']
         return ([diag_entry, diag_entry], [np.zeros(1), np.zeros(1)])
 
 
 def correlate(vec: ([int], [np.ndarray])):
     normalized_difference = np.linalg.norm(
-        vec[1][0]-vec[1][1])/sqrt(len(vec[1][0]))
+        vec[1][0]-vec[1][1])
     return [vec[0][0], vec[0][1], normalized_difference]
 
 
@@ -37,7 +38,7 @@ def correlate_docs(wordlist: pd.DataFrame, doc_range: pd.Series = None) -> [((in
         if not docs.empty:
             with open(os.path.join(temporary_save_dir, f'data_{name_appendix}_{doc_corr_count}_{docs_count}.csv'), 'a+', newline='') as temp_file:
                 file_writer = csv.writer(temp_file, delimiter=',')
-                print('Start:',doc_corr_count,docs_count)
+                print('Start:', doc_corr_count, docs_count)
                 file_writer.writerow(['DOC1', 'DOC2', 'Distance'])
                 for idx, i in enumerate(docs):
                     for j in docs_to_correlate:
@@ -47,7 +48,7 @@ def correlate_docs(wordlist: pd.DataFrame, doc_range: pd.Series = None) -> [((in
                             concat_wl, hot_encode(concat_wl)))
                         file_writer.writerow([x for x in two_doc_corr])
                     docs = docs[docs != i]
-                print('Finish:',doc_corr_count,docs_count)
+                print('Finish:', doc_corr_count, docs_count)
 
 
 def correlate_docs_single_thread(wordlist: pd.DataFrame, doc_range: pd.Series = None) -> [((int, int), np.float64)]:
@@ -64,5 +65,4 @@ def correlate_docs_single_thread(wordlist: pd.DataFrame, doc_range: pd.Series = 
                     two_doc_corr = correlate(aggregate_by_docno(
                         concat_wl, hot_encode(concat_wl)))
                     file_writer.writerow([x for x in two_doc_corr])
-                docs = docs.drop(docs == i)
-
+                docs = docs[docs != i]
--- evaluation_script.py
+++ evaluation_script.py
@@ -13,44 +13,72 @@ import os
 import sys
 
 
+def set_multithread(series):
+    if len(series) > cores_to_use:
+        apply_multithread = True
+    else:
+        apply_multithread = False
+
+
+def save_data(file_name: str):
+    all_files = glob.glob(os.path.join(
+        temporary_save_dir, "*.csv"))
+    if all_files:
+        total_file = pandas.concat((pandas.read_csv(
+            f, engine='python', on_bad_lines='warn', quoting=csv.QUOTE_NONE) for f in all_files), ignore_index=True)
+        total_file.to_csv(os.path.join(
+            save_dir, f'data_{file_name}.csv'))
+
+
+def clean_tmp_data():
+    all_files = glob.glob(os.path.join(
+        temporary_save_dir, "*.csv"))
+    for f in all_files:
+        os.remove(f)
+
+
+def prepare_sets(dictionary: dict, frame: str, category: str, value: str, wl: pandas.DataFrame):
+    filtered_sets, filtered_wl = data_manipulation_helper.filter_sdDicts(
+        dictionary, frame, category, value, wl)
+    filtered_wl = filtered_wl[filtered_wl['ATTACH_NO'] == '1']
+    series = filtered_sets['headers']['DOC_NUMBER']
+    return filtered_sets, filtered_wl, series
+
+
+def calculate_series_correlation(filtered_wl: pandas.DataFrame, series):
+    if apply_multithread:
+        frozen_wl_correlator = functools.partial(
+            calc_utility.correlate_docs, filtered_wl)
+        sliced_series_list = data_manipulation_helper.binomial_equipartition(
+            series, cores_to_use)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=cores_to_use) as executor:
+            executor.map(frozen_wl_correlator, sliced_series_list)
+    else:
+        calc_utility.correlate_docs_single_thread(filtered_wl, series)
+
+
 def main():
     wl = data_loader.load_all_wordlists()
     sd = data_loader.SapData(
         data_loader.path_to_sap_data, data_loader.tangro_om, wl, 0.7)
+
     ag_counts = sd.__train_dict['headers']['SOLD_TO'].value_counts()
     mat_counts = sd.__train_dict['items']['MATERIAL'].value_counts()
-    filter_list = [[
+    eval_filter_list = [[
         'headers', 'SOLD_TO', list(ag_counts.index)], ['items', 'MATERIAL', list(mat_counts.index)]]
 
-    for sd_set in filter_list:
-        for count in sd_set[2]:
-            name_appendix = sd_set[1] + '_' + str(count)
-            filtered_sets, filtered_wl = data_manipulation_helper.filter_sdDicts(
-                sd.__train_dict, sd_set[0], sd_set[1], count, wl)
-            filtered_wl = filtered_wl[filtered_wl['ATTACH_NO'] == '1']
-            series = filtered_sets['headers']['DOC_NUMBER']
-
-            if len(series) >  cores_to_use:
-                print(len(series))
-                frozen_wl_correlator = functools.partial(
-                    calc_utility.correlate_docs, filtered_wl)
-                sliced_series_list = data_manipulation_helper.equipartition(
-                    series, cores_to_use)
-                with concurrent.futures.ProcessPoolExecutor(max_workers=cores_to_use) as executor:
-                    executor.map(frozen_wl_correlator, sliced_series_list)
-            elif len(series) <= cores_to_use  and len(series) > 1:
-                frozen_wl_correlator = functools.partial(
-                    calc_utility.correlate_docs_single_thread, filtered_wl)
-                frozen_wl_correlator(series)
-            all_files = glob.glob(os.path.join(
-                temporary_save_dir, "*.csv"))
-            if all_files:
-                total_file = pandas.concat((pandas.read_csv(
-                    f, engine='python', on_bad_lines='warn', quoting=csv.QUOTE_NONE) for f in all_files), ignore_index=True)
-                total_file.to_csv(os.path.join(
-                    save_dir, f'data_{name_appendix}.csv'))
-                for f in all_files:
-                    os.remove(f)
+    for sd_set in eval_filter_list:
+        frame = sd_set[0]
+        category = sd_set[1]
+        values = sd_set[2]
+        for count in values:
+            filtered_sets, filtered_wl, series = prepare_sets(
+                sd.__train_dict, frame, category, count, wl)
+            set_multithread(series)
+            calculate_series_correlation(filtered_wl, series)
+            file_name = str(category) + '_' + str(count)
+            save_data(file_name)
+            clean_tmp_data()
 
 
 if __name__ == '__main__':
