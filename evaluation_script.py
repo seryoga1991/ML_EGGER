3a1ecd1e5600a3453fdc68d6a1485ae280efbf77
DESKTOP-N14U5N0\Sergej <agoyyes@web.de>
2021-11-15 15:27:27 +0100

    Spam Filter gedebuggt

--- evaluation_script.py
+++ evaluation_script.py
@@ -6,7 +6,7 @@ import data_loader
 import concurrent.futures
 import functools
 from multiprocessing import Process, Manager
-import config as cfg 
+import config as cfg
 from file_mover import move_spam
 import csv
 import glob
@@ -101,44 +101,53 @@ def classify_docs():
     return classified_dict
 
 
-def prepare_structs_for_comparisson(debitor_dict,debitor,filter_dict,wordlist):
-        exemplary_non_spam_doc = debitor_dict[debitor][1][0]
-        filtered_dict = data_manipulation_helper.filter_sdDicts(filter_dict,'headers','SOLD_TO',debitor,wordlist)
-        docs_to_compare = [filtered_dict['headers'][cfg.sd_key_val_pair['headers']],pandas.Series(exemplary_non_spam_doc),pandas.Series([]),pandas.Series([])]
-        wl = wordlist[wordlist['DOC_NUMBER'].isin(docs_to_compare[0])]
-        return docs_to_compare,wl
+def prepare_structs_for_comparisson(debitor_dict, debitor, filter_dict, wordlist):
+    exemplary_non_spam_doc = debitor_dict[debitor][1][0]
+    filtered_dict = data_manipulation_helper.filter_sdDicts(
+        filter_dict, 'headers', 'SOLD_TO', debitor, wordlist)
+    docs_for_debitor = filtered_dict.get('headers')[cfg.sd_key_val_pair.get(
+        'headers')]
+    compare_to_doc = pandas.Series(exemplary_non_spam_doc)
+    docs_for_correlate = [docs_for_debitor,
+                          compare_to_doc, pandas.Series([]), pandas.Series([])]
+    wl = wordlist[wordlist['DOC_NUMBER'].isin(docs_for_correlate[0])]
+    return docs_to_compare, wl
 
-def correlacte_docs(wordlist,docs_to_compare):
-        calc_utility.correlate_docs(wordlist,docs_to_compare)
-        file_path = glob.glob(os.path.join(cfg.temporary_save_dir, cfg.temp_name))
-        file = pandas.read_csv(file_path[0])
-        return file
+
+def correlacte_docs(wordlist, docs_to_compare):
+    calc_utility.correlate_docs(wordlist, docs_to_compare)
+    file_path = glob.glob(os.path.join(cfg.temporary_save_dir, cfg.temp_name))
+    file = pandas.read_csv(file_path[0])
+    return file
+
 
 def determine_all_spam(classified_dict):
     wl = data_loader.load_wordlists()
-    sd = data_loader.SapData(cfg.path_to_sap_data,data_loader.tangro_om,wl, 0.)
+    sd = data_loader.SapData(cfg.path_to_sap_data,
+                             data_loader.tangro_om, wl, 0.)
     spam_list = []
     non_spam_list = []
     cfg.name_appendix = 'temp_corr'
     for key in classified_dict:
-        docs_to_compare,wordlist = prepare_structs_for_comparisson(debitor_dict = classified_dict, debitor = key, filter_dict = sd.__train_dict, wordlist = wl)
-        file = correlacte_docs(wordlist,docs_to_compare)
-        temp_spam_list , temp_non_spam_list = data_manipulation_helper.get_spam_by_correlation_scores(file,0.8)
+        docs_to_compare, wordlist = prepare_structs_for_comparisson(
+            debitor_dict=classified_dict, debitor=key, filter_dict=sd.__train_dict, wordlist=wl)
+        file = correlacte_docs(wordlist, docs_to_compare)
+        temp_spam_list, temp_non_spam_list = data_manipulation_helper.get_spam_by_correlation_scores(
+            file, 0.8)
         spam_list.append(temp_spam_list)
         non_spam_list.append(temp_non_spam_list)
-    
+
     spam_list = [spam for sublist in spam_list for spam in sublist]
     return spam_list
 
 
-
-
-def get_rid_of_spam(data_dir = cfg.path_to_wordlists):
+def get_rid_of_spam(data_dir=cfg.path_to_wordlists):
     spam_list = []
     classified_dict = classify_docs()
     spam_list = determine_all_spam(classified_dict)
     move_spam(spam_list)
     clean_tmp_data()
+
 
 if __name__ == '__main__':
     get_rid_of_spam()
